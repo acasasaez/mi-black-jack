ffeedaa3d07fdd92b357b262751400b1025cdd19
acasasaez <acasasae@myuax.com>
2021-11-23 15:21:07 +0100

    diccionario

---
+++ juego.py
@@ -0,0 +1,14 @@
+#Juego del black jack:
+cartas ={chr(0x1f0a): 11,
+         chr(0x1fa2): 2,
+         chr (0x1f03): 3,
+         chr (0x1f0a4):4,
+         chr(0x1f0a5): 5,
+         chr(0x1f0a6): 6,
+         chr(0x1f0a7): 7,
+         chr(0x1f08): 8,
+         chr (0x1f0a9): 9,
+         chr(0x1f0aa):10,
+         chr(0x1f0ab):10,
+         chr(0x1f0ad):10,
+         chr(0x1f0ae): 10 }
